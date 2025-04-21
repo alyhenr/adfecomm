dec148a3e8e70970a53b5777edbb531d79acdbbf
alyhenr <alyssonhra99@gmail.com>
2025-04-21 11:43:58 -0300

    hotfix: rest controller anotation missing on LoginController

--- src/main/java/com/adfecomm/adfecomm/controller/LoginController.java
+++ src/main/java/com/adfecomm/adfecomm/controller/LoginController.java
@@ -15,12 +15,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@RestController
 public class LoginController {
     @Autowired
     AuthenticationManager authenticationManager;
